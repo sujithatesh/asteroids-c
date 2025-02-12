5d36c4539e1431ca04ff429856b36851d5872e26
Sujith Atesh <sujithatesh@gmail.com>
2025-02-12 09:47:57 +0530

    fix: loop code and merged into one entity

--- asteroids.c
+++ asteroids.c
@@ -1,217 +1,100 @@
 #include <raylib.h>
-#include <assert.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <sys/mman.h>
+#include <time.h>
 
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 600
-#define TILE_SIZE 40
-#define BULLET_SIZE 4
+#define W 800
+#define H 450
+#define MAX_ASTEROIDS 10
+#define MAX_BULLETS 20
 
+typedef struct { Vector2 pos, vel; float size; bool active; } Entity;
 
-typedef struct Arena{
-    void* memory;
-    size_t used;
-    size_t capacity;
-}Arena;
+int main() {
+    InitWindow(W, H, "Asteroids");
+    SetTargetFPS(60);
+    srand(time(0));
 
+    Entity player = {{W/2, H/2}, {0, 0}, 20, true};
+    Entity asteroids[MAX_ASTEROIDS];
+    Entity bullets[MAX_BULLETS];
+    int asteroidCount = 0, bulletCount = 0;
 
-Arena arena_init(size_t size){
-    void* memory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-    return (Arena){
-	.memory = memory,
-	.capacity = size,
-	.used = 0
-    };
-}
-
-void* arena_alloc(Arena* arena, size_t size){
-    if(arena->used + size > arena->capacity){
-	arena->used = 0;
+    for (int i = 0; i < MAX_ASTEROIDS; i++) {
+        asteroids[i].size = (float)(rand() % 30 + 15);
+        asteroids[i].pos = (Vector2){(float)(rand() % W), (float)(rand() % H)};
+        asteroids[i].vel = (Vector2){(float)(rand() % 200 - 100), (float)(rand() % 200 - 100)};
+        asteroids[i].active = true;
+        asteroidCount++;
     }
-    void* ptr =  (char*)arena->memory + arena->used;
-    arena->used += size;
-    return ptr;
-}
 
-void arena_destroy(Arena* arena){
-    munmap(arena->memory, arena->capacity);
-    arena->memory = NULL;
-    arena->used = 0;
-    arena->capacity = 0;
-}
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
 
-typedef enum Direction{ W, A, S, D} Direction;
+        // Player movement/rotation (simplified)
+        if (IsKeyDown(KEY_W)) player.vel.y -= 100 * dt;
+        if (IsKeyDown(KEY_S)) player.vel.y += 100 * dt;
+        if (IsKeyDown(KEY_A)) player.vel.x -= 100 * dt;
+        if (IsKeyDown(KEY_D)) player.vel.x += 100 * dt;
+        player.pos.x += player.vel.x * dt;
+        player.pos.y += player.vel.y * dt;
+        player.vel = (Vector2){0,0}; // Reset velocity each frame for simple movement
 
-typedef struct player{
-    Vector2 ver1;
-    Vector2 ver2;
-    Vector2 ver3;
-} player;
+        // Keep player on screen (wrap around)
+        if (player.pos.x > W) player.pos.x = 0;
+        if (player.pos.x < 0) player.pos.x = W;
+        if (player.pos.y > H) player.pos.y = 0;
+        if (player.pos.y < 0) player.pos.y = H;
 
-typedef struct bullet{
-    Vector2 pos;
-    Vector2 vec;
-    float speed;
-    float ttl;
-} bullet;
+        // Shooting
+        if (IsKeyPressed(KEY_SPACE) && bulletCount < MAX_BULLETS) {
+            bullets[bulletCount] = (Entity){player.pos, {0, -300}, 4, true};  // Simplified bullet velocity
+            bulletCount++;
+        }
 
-typedef struct bulletType{
-    int ttl;
-    float rate;
-} bulletType;
+        // Bullet update
+        for (int i = 0; i < bulletCount; i++) {
+            if (bullets[i].active) {
+                bullets[i].pos.y += bullets[i].vel.y * dt;
+                if (bullets[i].pos.y < 0) bullets[i].active = false; // Bullet off-screen
+            }
+        }
 
-void movDir(player* p, Direction d, float scale, float dT){
-    switch (d) {
-	case W:
-	    p->ver1.y -= scale * dT;
-	    p->ver2.y -= scale * dT;
-	    p->ver3.y -= scale * dT;
-	    break;
-	case A:
-	    p->ver1.x -= scale * dT;
-	    p->ver2.x -= scale * dT;
-	    p->ver3.x -= scale * dT;
-	    break;
-	case S:
-	    p->ver1.y += scale * dT;
-	    p->ver2.y += scale * dT;
-	    p->ver3.y += scale * dT;
-	    break;
-	case D:
-	    p->ver1.x += scale * dT;
-	    p->ver2.x += scale * dT;
-	    p->ver3.x += scale * dT;
-	    break;
-	default:
-	    assert("Not a correct direction");
+        // Asteroid update
+        for (int i = 0; i < asteroidCount; i++) {
+            if (asteroids[i].active) {
+                asteroids[i].pos.x += asteroids[i].vel.x * dt;
+                asteroids[i].pos.y += asteroids[i].vel.y * dt;
+
+                // Keep asteroids on screen (wrap around)
+                if (asteroids[i].pos.x > W) asteroids[i].pos.x = 0;
+                if (asteroids[i].pos.x < 0) asteroids[i].pos.x = W;
+                if (asteroids[i].pos.y > H) asteroids[i].pos.y = 0;
+                if (asteroids[i].pos.y < 0) asteroids[i].pos.y = H;
+            }
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        // Draw player (triangle)
+        DrawTriangleLines((Vector2){player.pos.x, player.pos.y - player.size},
+                         (Vector2){player.pos.x - player.size, player.pos.y + player.size},
+                         (Vector2){player.pos.x + player.size, player.pos.y + player.size}, WHITE);
+
+        // Draw bullets
+        for (int i = 0; i < bulletCount; i++) {
+            if (bullets[i].active) DrawCircleV(bullets[i].pos, bullets[i].size, WHITE);
+        }
+
+        // Draw asteroids
+        for (int i = 0; i < asteroidCount; i++) {
+            if (asteroids[i].active) DrawCircleV(asteroids[i].pos, asteroids[i].size, GRAY);
+        }
+
+        EndDrawing();
     }
-}
 
-void DrawPlayer(Vector2* mousePos, player* p1, float theta){
-    DrawLine(p1->ver1.x , p1->ver1.y, mousePos->x, mousePos->y, RED);
-    DrawCircle(p1->ver2.x, p1->ver2.y, 4, GREEN); 
-    DrawCircle(p1->ver3.x, p1->ver3.y, 4, BLUE); 
-
-    p1->ver2.x = p1->ver1.x - TILE_SIZE * cosf(theta - 1.0f * PI / 6.0f);
-    p1->ver2.y = p1->ver1.y - TILE_SIZE * sinf(theta - 1.0f * PI / 6.0f);
-
-    p1->ver3.x = p1->ver1.x - TILE_SIZE * cosf(theta + 1.0f * PI / 6.0f);
-    p1->ver3.y = p1->ver1.y - TILE_SIZE * sinf(theta + 1.0f * PI / 6.0f);
-
-    DrawTriangleLines(p1->ver1, p1->ver2, p1->ver3, RAYWHITE);
-}
-
-void drawBullets(bullet* b, int* subCounter, float dT, int i){
-    b->ttl -= dT;
-    if(b->ttl < 0){
-	*subCounter = i+1 ;
-    }
-    b->pos.x += b->vec.x * b->speed * dT;
-    b->pos.y += b->vec.y * b->speed * dT;
-    DrawRectangleV(b->pos, (Vector2){.x = BULLET_SIZE, .y = BULLET_SIZE}, WHITE);
-}
-
-int main(void){
-    float speed = 350;
-    int maxBullets = 40;
-
-    player p1 = {.ver1 = {.x = 300, .y = 300},
-	.ver2 = {.x = 300 - 0.5 * TILE_SIZE, .y = 300 + 0.86602540378 * TILE_SIZE},
-	.ver3 = {.x = 300 + 0.5 * TILE_SIZE, .y = 300 + 0.86602540378 * TILE_SIZE}
-    };
-
-    bulletType newbulletType[] = { 
-	{ .ttl = 1, .rate = 1 * 0.1},
-	{ .ttl = 2, .rate = 2 * 0.1},
-	{ .ttl = 4, .rate = 3 * 0.1}
-    };
-
-    float bulletTicker = 0;
-    int addCounter = 0;
-    int subCounter = 0;
-    bullet* bulletArray;
-    Arena bulletArena = arena_init(maxBullets * sizeof(bullet));
-    float dT = 0;
-
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Init Window");
-    while(!WindowShouldClose()){
-	BeginDrawing(); 
-	ClearBackground(BLACK);
-
-	dT = GetFrameTime();
-	bulletTicker += dT;
-	Vector2 mousePos = GetMousePosition();
-	float theta = atan2f((mousePos.y - p1.ver1.y), (mousePos.x - p1.ver1.x));
-
-	if(IsKeyDown(KEY_W)) {
-	    movDir(&p1, W, speed, dT);
-	}
-	if(IsKeyDown(KEY_A)) {
-	    movDir(&p1, A, speed, dT);
-	}
-	if(IsKeyDown(KEY_S)) {
-	    movDir(&p1, S, speed, dT);
-	}
-	if(IsKeyDown(KEY_D)) {
-	    movDir(&p1, D, speed, dT);
-	}
-
-	if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
-	    if(bulletTicker > newbulletType[1].rate){
-		if(addCounter >= maxBullets){
-		    addCounter = 0;
-		}
-		bulletTicker = 0;
-		bullet* newbullet = arena_alloc(&bulletArena, sizeof(bullet));
-		newbullet->pos = (Vector2){.x = p1.ver1.x - BULLET_SIZE / 2, .y = p1.ver1.y - BULLET_SIZE / 2};
-		newbullet->vec = (Vector2){.x = cosf(theta), .y = sinf(theta)};
-		newbullet->speed = 700;
-		newbullet->ttl = newbulletType[2].ttl;
-		addCounter++;
-		bulletArray = bulletArena.memory;
-		if(addCounter > maxBullets) addCounter = 0;
-	    }
-	}
-
-	if(addCounter > 0){
-#if 0
-	    if(bulletArray[i].pos.y > SCREEN_HEIGHT){
-		bulletArray[i].pos.y = 0;
-	    }
-	    if(bulletArray[i].pos.x > SCREEN_WIDTH){
-		bulletArray[i].pos.x = 0;
-	    }
-	    if(bulletArray[i].pos.x < 0){
-		bulletArray[i].pos.x = SCREEN_WIDTH;
-	    }
-	    if(bulletArray[i].pos.y < 0){
-		bulletArray[i].pos.y = SCREEN_HEIGHT;
-	    }
-#endif
-	    if(addCounter < subCounter){
-		for(size_t i = subCounter; i < maxBullets; i++){
-		    drawBullets(&bulletArray[i], &subCounter, dT, i);
-		    if(subCounter > maxBullets) subCounter = 0;
-		}
-		for(size_t i = 0; i < addCounter; i++){
-		    drawBullets(&bulletArray[i], &subCounter, dT, i);
-		}
-	    }
-	    else{
-		for(size_t i = subCounter; i < addCounter; i++){
-		    drawBullets(&bulletArray[i], &subCounter, dT, i);
-		}
-	    }
-	}
-	printf("%d, %d\n", addCounter, subCounter);
-
-	DrawPlayer(&mousePos, &p1, theta);
-	DrawFPS(20, 20);
-	EndDrawing();
-    }
-    arena_destroy(&bulletArena);
+    CloseWindow();
     return 0;
 }
